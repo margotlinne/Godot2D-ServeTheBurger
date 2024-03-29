98dff418a420ea20e4ea98d888ef24e3967f8b0d
user <migang1023@gmail.com>
2024-03-29 21:45:26 +0900

    [Edit/Add]
    new images, set main menu and game UI

--- script/Menu.gd
+++ script/Menu.gd
@@ -7,9 +7,10 @@ func _on_play_btn_pressed():
 	get_tree().change_scene_to_file("res://game.tscn")
 
 
-func _on_options_btn_pressed():
-	pass # Replace with function body.
-
 
 func _on_exit_btn_pressed():
 	get_tree().quit()
+
+
+func _on_option_btn_pressed():
+	pass # Replace with function body.
